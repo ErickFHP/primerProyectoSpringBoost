c74bbd5fe51bbb31d8ee222d1bbb10c799762797
Erick Hernández <84101451+ErickFHP@users.noreply.github.com>
2024-04-05 14:59:35 -0600

    Proyecto terminado
    Se presenta el proyecto por medio de un RestController. Esto no permite usar thymeleaf, aunque se añadió una página html en templates llamada tareas en la que, en caso de solo usar Controller, se puede consultar todas las tareas.

---
+++ primerProyecto/src/main/java/com/api/primerProyecto/models/TareaModel.java
@@ -0,0 +1,59 @@
+package com.api.primerProyecto.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tarea")
+public class TareaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String descr;
+
+    private String estado;
+
+    private String prioridad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescr() {
+        return descr;
+    }
+
+    public void setDescr(String descr) {
+        this.descr = descr;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+}
